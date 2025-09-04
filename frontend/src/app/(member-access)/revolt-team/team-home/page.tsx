b486a84ca00db798732db2e2c7aac6a7c9dffe9c
Usman Mahmood <usmanmahmood254@gmail.com>
2025-09-04 12:15:03 -0600

    Begun ReVOLT Member Content

--- frontend/src/app/(member-access)/revolt-team/team-home/page.tsx
+++ frontend/src/app/(member-access)/revolt-team/team-home/page.tsx
@@ -5,7 +5,7 @@ import revolt_style from '@/styles/ReVOLT_Team.module.scss';
 export default function ReVOLT_Home_Page() {
 
   return (
-    <div className={revolt_style.revolt_container} style={{display: "flex", height: "100vh", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+    <div className={revolt_style.revolt_container}>
       <h1>This part of the site is not ready yet :(</h1>
     </div>
   );
