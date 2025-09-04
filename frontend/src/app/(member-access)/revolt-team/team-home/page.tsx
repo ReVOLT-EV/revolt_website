7ce537efa8b0a39524c08879b0097c7f2a7dd9be
Usman Mahmood <usmanmahmood254@gmail.com>
2025-09-04 14:50:08 -0600

    More Updates

--- frontend/src/app/(member-access)/revolt-team/team-home/page.tsx
+++ frontend/src/app/(member-access)/revolt-team/team-home/page.tsx
@@ -5,18 +5,21 @@ import Link from 'next/link';
 import revolt_style from '@/styles/ReVOLT_Team.module.scss';
 
 export default function ReVOLT_Home_Page() {
-
   return (
     <div className={revolt_style.revolt_container}>
       <div className={revolt_style.block}>
         <div className={revolt_style.page_1_link}>
           <div>
-            <Link href=""></Link>
+            <Link href="/">
+              ReVOLT Systems
+            </Link>
           </div>
         </div>
         <div className={revolt_style.page_2_link}>
           <div>
-            <Link href=""></Link>
+            <Link href="">
+              ReVOLT Wiki
+            </Link>
           </div>
         </div>
       </div>
