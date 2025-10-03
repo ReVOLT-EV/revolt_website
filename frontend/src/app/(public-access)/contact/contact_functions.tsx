afb0fb43b177b4bfc7748e2fbf7f41fffd73edc9
Usman Mahmood <usmanmahmood254@gmail.com>
2025-10-03 14:11:37 -0600

    Updated the EmailJS component

--- frontend/src/app/(public-access)/contact/contact_functions.tsx
+++ frontend/src/app/(public-access)/contact/contact_functions.tsx
@@ -17,7 +17,7 @@ export default function ContactPage() {
     setStatus("Sending...");
 
     try {
-      const res = await fetch("http://localhost:4000/send", {
+      const res = await fetch("https://revolt-website-emailsys.onrender.com/send", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
